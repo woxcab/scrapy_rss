70e546b6c0cd3581630102c59219b80fd2cd4a89
woxcab <woxcab@users.noreply.github.com>
2020-06-06 17:12:20 +0300

    Remove deprecated script

--- report_coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run tests, generate coverage report and open it on a browser
-#
-# Requires: coverage 3.3 or above from http://pypi.python.org/pypi/coverage
-
-PYTHONPATH="$PWD:$PYTHONPATH" coverage run $(which trial) --reporter=text tests
-coverage html -i
-python -m webbrowser coverage_report/index.html
