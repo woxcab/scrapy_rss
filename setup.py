0d98bb7a539d48338b55c5761a152ab160189ea5
woxcab <woxcab@users.noreply.github.com>
2022-04-11 12:33:07 +0300

    Restore Python 3.3 support
    This reverts commit 2ea9890b39af668f7b09ecb700ea3edc05a3e275.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ with open('README.rst') as readme:
         packages=find_packages(exclude=('tests',)),
         include_package_data=True,
         zip_safe=False,
-        python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+        python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
         classifiers=[
             'Framework :: Scrapy',
             'Development Status :: 4 - Beta',
@@ -58,6 +58,7 @@ with open('README.rst') as readme:
             'Programming Language :: Python :: 2',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
