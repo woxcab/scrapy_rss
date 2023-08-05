20166820f9bcab0349f3662ebb8cbf81a82245be
woxcab <woxcab@users.noreply.github.com>
2023-08-05 23:45:46 +0300

    Add the Python3.11 classifier
    [skip ci]

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ with open('README.rst') as readme:
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
             'Topic :: Internet :: WWW/HTTP',
             'Topic :: Software Development :: Libraries :: Python Modules',
         ],
