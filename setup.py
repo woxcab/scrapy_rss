38e7ac91139bbbbfd42b41763103546eac3956e2
woxcab <woxcab@users.noreply.github.com>
2025-09-14 14:06:10 +0300

    Mark project as stable

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('README.rst') as readme:
         python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
         classifiers=[
             'Framework :: Scrapy',
-            'Development Status :: 4 - Beta',
+            'Development Status :: 5 - Production/Stable',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
