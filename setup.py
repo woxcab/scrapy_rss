f983c7c619000695b5fa1946af062936371c64b4
woxcab <woxcab@users.noreply.github.com>
2025-09-15 18:12:29 +0300

    Exclude tests.elements module on building

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ with open('README.rst') as readme:
         description='RSS Tools for Scrapy Framework',
         long_description=readme.read(),
         license='BSD',
-        packages=find_packages(exclude=('tests',)),
+        packages=find_packages(exclude=('tests', 'tests.elements')),
         include_package_data=True,
         zip_safe=False,
         python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
